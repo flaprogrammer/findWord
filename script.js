6087655534e042286c4f6731d5452c183031d2ff
flaprogrammer <flaprogrammer@gmail.com>
2016-04-14 07:50:08 +0300

    combinatorics.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
+var Combinatorics = require('js-combinatorics');
 
 var arr = [];
+var found = [];
 fs.readFile('./pldb-win.txt', 'utf8', (err, data) => {
   if (err) throw err;
   handleData(data);
@@ -9,7 +11,6 @@ fs.readFile('./pldb-win.txt', 'utf8', (err, data) => {
 function handleData(res) {
 	arr = res.split('\r\n')
 	.map((el) => el.replace(/\s|\./g, ''));
-	findWord('свинья');
 	generateWords(5, ['у','р','з','г','ч','ь','ч','м','у','н','щ','е','а','ф','п','т','т','т',]);
 }
 
@@ -19,10 +20,25 @@ function findWord(word) {
 
 
 function generateWords(len, letters) {
-	var a = 'к'+generateWord();
-	if(findWord(a)) console.log(a);
+	var w;
+	var cmb2;
+	var cmb = Combinatorics.combination(letters, len);
+	console.log(cmb.toArray().length);
+	var i = 0;
+	while(a = cmb.next()) {
+		cmb2 = Combinatorics.permutation(a);
+		while(b = cmb2.next()) {
+			w = b.join('');
+			if(findWord("к"+w)) {
+				console.log("к"+w);
+				found.push("к"+w);
+			}
+		}
+	}
+	fs.writeFile('res.txt', found.join('\n '), (err) => {
+	  if (err) throw err;
+	  console.log('It\'s saved!');
+	});
+
 }
 
-function generateWord() {
-
-}
